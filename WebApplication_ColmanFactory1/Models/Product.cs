909e3ea65106aa2a01c4ac70ce3fb24c2a7d9b49
IdoGo15 <idogolan15@gmail.com>
2021-07-25 08:54:11 +0300

    Product model

--- WebApplication_ColmanFactory1/Models/Product.cs
+++ WebApplication_ColmanFactory1/Models/Product.cs
@@ -9,42 +9,19 @@ namespace WebApplication_ColmanFactory1.Models
 {
     public class Product
     {
-        // Product ID
         [Key]
-        [Required(ErrorMessage = "You must enter a Product ID")]
         public int ProductID { get; set; }
 
-        // Product Name
-        [DisplayName("Name")]
-        [Required(ErrorMessage = "You must enter a name")]
-        [RegularExpression("^[A-Z][a-zA-Z ]*$", ErrorMessage = "The body must contains only letters and start with one uppercase letter")]
-        public string ProductName { get; set; }
+        public string Name { get; set; }
 
-        [DisplayName("Image")]
-        public string ProductImagePath { get; set; }
+        public string ImagePath { get; set; }
 
-        [DisplayName("Price")]
-        [Required]
-        [Range(0, 500)]
-        [DataType(DataType.Currency)]
-        public double ProductPrice { get; set; }
+        public double Price { get; set; }
 
         public bool IsOnSale { get; set; }
 
-        public bool IsInStock { get; set; }
-
-        //Required]
-        //public List<Int32> ProductSizeList { get; set; }
-
-        [DisplayName("Description")]
-        public string ProductDescription { get; set; }
-
-        //one -> many : Category -> Product
         public int CategoryID { get; set; }
+
         public Category Category { get; set; }
-
-
-        //many <-> many : Cart <-> Product
-        public List<Cart> ProductCartList { get; set; }
     }
 }
