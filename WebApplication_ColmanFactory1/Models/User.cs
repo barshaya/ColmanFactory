fd653ee57f38acd0a54a0f20ccd0689e0d4c6a27
IdoGo15 <idogolan15@gmail.com>
2021-07-25 18:49:53 +0300

    add User model

---
+++ WebApplication_ColmanFactory1/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_ColmanFactory1.Models
+{
+    public enum UserType
+    {
+        Client,
+        Admin
+    }
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public UserType Type { get; set; } = UserType.Client;
+
+        public Cart Cart { get; set; } = new Cart();
+    }
+}
