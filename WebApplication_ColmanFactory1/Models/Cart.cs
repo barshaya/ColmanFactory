c080ed65ad97ccf6364eecc738ca98a202526666
IdoGo15 <idogolan15@gmail.com>
2021-07-25 09:10:22 +0300

    Cart model

--- WebApplication_ColmanFactory1/Models/Cart.cs
+++ WebApplication_ColmanFactory1/Models/Cart.cs
@@ -8,22 +8,7 @@ namespace WebApplication_ColmanFactory1.Models
 {
     public class Cart
     {
-        [Key]
-        public int CartId { get; set; }
-
-        public string CartName { get; set; }
-
-        // one <-> one : Cart <-> User
-        public int UserID { get; set; }
-        public User User { get; set; }
-
-        //many <-> many : Cart <-> Product 
-
-        public List<Product> Products { get; set; }
-
-        [DataType(DataType.Currency)]
-        public int TotalAmount { get; set; }
-
-
+        public string CartId { get; set; }
+        public List<CartItem> CartItems { get; set; }
     }
 }
