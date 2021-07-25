8cabde36f3077e94561b8d06d6d01a554eba40d3
IdoGo15 <idogolan15@gmail.com>
2021-07-25 09:07:07 +0300

    CartItem model

---
+++ WebApplication_ColmanFactory1/Models/CartItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_ColmanFactory1.Models
+{
+    public class CartItem
+    {
+        [Key]
+        public int CartItemId { get; set; }
+        public string CartId { get; set; }
+        public Product product { get; set; }
+        public int Amount { get; set; }
+    }
+}
